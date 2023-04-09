bcf62033bc03254c9a8075e0aa706b49b03261b4
klesio <klesio.dev@gmail.com>
2023-04-09 17:34:04 -0300

    feat(camera): test switch camera

--- src/pages/camera.tsx
+++ src/pages/camera.tsx
@@ -1,13 +1,39 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 type CameraProps = {
   onStream: (stream: MediaStream) => void;
   onError: (error: Error) => void;
 };
 
+type VideoDevice = {
+  deviceId: string;
+  label: string;
+};
+
 export const Camera = ({ onStream, onError }: CameraProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const [stream, setStream] = useState<MediaStream | null>(null);
+  const [devices, setDevices] = useState<VideoDevice[]>([]);
+  const [selectedDevice, setSelectedDevice] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getDevices = async () => {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const videoDevices = devices.filter(
+        (device) => device.kind === 'videoinput'
+      );
+      setDevices(
+        videoDevices.map((device) => ({
+          deviceId: device.deviceId,
+          label: device.label || `Camera ${videoDevices.indexOf(device) + 1}`,
+        }))
+      );
+      if (videoDevices.length > 0) {
+        setSelectedDevice(videoDevices[0].deviceId);
+      }
+    };
+    getDevices();
+  }, []);
 
   const handleStartClick = async () => {
     try {
@@ -35,12 +61,38 @@ export const Camera = ({ onStream, onError }: CameraProps) => {
     }
   };
 
+  const handleDeviceChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedDevice(event.target.value);
+  };
+
+  const switchCamera = () => {
+    const index = devices.findIndex(
+      (device) => device.deviceId === selectedDevice
+    );
+    const nextIndex = (index + 1) % devices.length;
+    setSelectedDevice(devices[nextIndex].deviceId);
+  };
+
   return (
-    <div className='flex flex-col gap-4 p-4'>
+    <div className='flex flex-col gap-4 p-4 justify-start'>
       <div className='bg-gray-400 h-96'>
         <video ref={videoRef} autoPlay />
       </div>
-
+      <div className='flex flex-col justify-center w-full gap-3'>
+        <button
+          onClick={switchCamera}
+          className='bg-green-400 w-1/2 p-2 rounded '
+        >
+          Switch Camera
+        </button>
+        <select className='w-full' value={selectedDevice || ''} onChange={handleDeviceChange}>
+          {devices.map((device) => (
+            <option key={device.deviceId} value={device.deviceId}>
+              {device.label}
+            </option>
+          ))}
+        </select>
+      </div>
       <div className='flex gap-4 justify-between text-white'>
         <button
           onClick={handleStartClick}
